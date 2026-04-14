f73206d526c4085722dd703ca241ab684cd76442
aarush <aarushk0207@gmail.com>
2026-04-14 17:58:47 +0530

    fix CORS headers and Vercel routing

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-secret']
 }))
 app.use(express.json({ limit: '10kb' }));
 
